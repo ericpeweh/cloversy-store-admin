81965582406a917c72cd2582d182072a6581bd20
Eric Prima Wijaya <ericpeweh@gmail.com>
2022-11-16 18:01:20 +0700

    change button base to LoadingButton (add loading prop)

--- components/Button/Button.styles.ts
+++ components/Button/Button.styles.ts
@@ -2,9 +2,9 @@
 import { styled } from "@mui/system";
 
 // Components
-import { Button } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
 
-export const CustomButton = styled(Button)(({ theme, ...props }) => ({
+export const CustomButton = styled(LoadingButton)(({ theme, ...props }) => ({
 	fontSize: props.size === "large" ? "1.7rem" : "1.5rem",
 	padding: props.size === "large" ? "1rem 2.8rem" : "0.6rem 2rem",
 	fontFamily: "var(--font-secondary)",
