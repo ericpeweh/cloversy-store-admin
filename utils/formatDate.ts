de24d2080f05f62487bb9b7597dc4a37ca299cc1
Eric Prima Wijaya <ericpeweh@gmail.com>
2022-11-17 23:45:19 +0700

    create date formatting utils fn

---
+++ utils/formatDate.ts
@@ -0,0 +1,12 @@
+// Dependencies
+import { DateTime } from "luxon";
+
+type StringDate = string;
+
+export const formatDateFull = (str: StringDate) => {
+	return DateTime.fromISO(str).setLocale("id").toFormat("dd LLLL yyyy, HH:mm 'WIB'");
+};
+
+export const formatDateStandard = (str: StringDate) => {
+	return DateTime.fromISO(str).setLocale("id").toFormat("dd/LL/yyyy");
+};
