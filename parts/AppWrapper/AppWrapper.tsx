f682fb091419c144c19d0470e4b9b34da50b5a6c
Eric Prima Wijaya <ericpeweh@gmail.com>
2022-11-19 15:54:34 +0700

    improve auth flow
    - add /auth to AppWrapper component
    - store more data in authSlice (full_name, email, profile_picture, email_verified)

--- parts/AppWrapper/AppWrapper.tsx
+++ parts/AppWrapper/AppWrapper.tsx
@@ -1,8 +1,13 @@
 // Dependencies
+import axios from "axios";
+import { useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 // Hooks
+import useDispatch from "../../hooks/useDispatch";
 
 // Action
+import { setCredentials } from "../../store/slices/authSlice";
 
 // Components
 import Header from "../Header/Header";
@@ -13,6 +18,36 @@ interface AppWrapperProps {
 }
 
 const AppWrapper = ({ children }: AppWrapperProps) => {
+	const dispatch = useDispatch();
+
+	const { isAuthenticated, user, getAccessTokenSilently } = useAuth0();
+
+	useEffect(() => {
+		if (isAuthenticated) {
+			const getToken = async () => {
+				const token = await getAccessTokenSilently();
+
+				dispatch(
+					setCredentials({
+						isAuth: true,
+						token,
+						full_name: user?.name ?? "",
+						email: user?.email ?? "",
+						profile_picture: user?.picture ?? "",
+						email_verified: user?.email_verified ?? false
+					})
+				);
+
+				await axios.get("http://localhost:5000/auth", {
+					headers: {
+						Authorization: `Bearer ${token}`
+					}
+				});
+			};
+			getToken();
+		}
+	}, [getAccessTokenSilently, isAuthenticated, dispatch, user]);
+
 	return (
 		<>
 			<Header />
--- store/slices/authSlice.ts
+++ store/slices/authSlice.ts
@@ -5,20 +5,37 @@ import type { PayloadAction } from "@reduxjs/toolkit";
 interface AuthState {
 	isAuth: boolean;
 	token: string;
+	full_name: string;
+	email: string;
+	profile_picture: string;
+	email_verified: boolean;
 }
 
 const initialState: AuthState = {
 	isAuth: false,
-	token: ""
+	token: "",
+	full_name: "",
+	email: "",
+	profile_picture: "",
+	email_verified: false
 };
 
 const authSlice = createSlice({
 	name: "auth",
 	initialState,
 	reducers: {
-		setCredentials: (state, { payload: { isAuth, token } }: PayloadAction<AuthState>) => {
+		setCredentials: (
+			state,
+			{
+				payload: { isAuth, token, full_name, email, profile_picture, email_verified }
+			}: PayloadAction<AuthState>
+		) => {
 			state.isAuth = isAuth;
 			state.token = token;
+			state.full_name = full_name;
+			state.email = email;
+			state.profile_picture = profile_picture;
+			state.email_verified = email_verified;
 		}
 	}
 });
