36dcb6b949a1467a2bc0cb296d4f7709fd1276ba
Eric Prima Wijaya <ericpeweh@gmail.com>
2022-11-19 15:57:45 +0700

    add UserListModal component based on UserPickerModal

---
+++ components/UserListModal/UserListModal.tsx
@@ -0,0 +1,72 @@
+// Dependencies
+import React from "react";
+
+// Styles
+import {
+	UserContainer,
+	UserContent,
+	UserInfo,
+	Username,
+	UserOptions,
+	UserPickerModalContainer,
+	ModalTitle,
+	Email,
+	UserImage,
+	UserPickerHeader
+} from "../UserPickerModal/UserPickerModal.styles";
+
+// Types
+import { Customer } from "../../interfaces";
+
+// Components
+import { Divider, Avatar } from "@mui/material";
+import CloseButton from "../CloseButton/CloseButton";
+
+interface UserListModalProps {
+	open: boolean;
+	onClose: () => void;
+	data: Partial<Customer>[];
+}
+
+const UserListModal = ({ open, onClose, data }: UserListModalProps) => {
+	return (
+		<UserPickerModalContainer open={open} onClose={onClose}>
+			<CloseButton
+				onClick={onClose}
+				sx={{
+					top: { xs: "1.5rem", sm: "2.5rem" },
+					right: { xs: "2rem", sm: "3rem" },
+					width: "3rem",
+					height: "3rem"
+				}}
+			/>
+			<UserPickerHeader>
+				<ModalTitle>Daftar customer</ModalTitle>
+			</UserPickerHeader>
+			<Divider />
+			<UserOptions>
+				{data?.length > 0 &&
+					data.map(user => (
+						<UserContainer key={user.id}>
+							<UserContent>
+								<UserImage>
+									<Avatar
+										src={user.profile_image}
+										alt="user profile"
+										imgProps={{ referrerPolicy: "no-referrer" }}
+										sx={{ height: "100%", width: "100%" }}
+									/>
+								</UserImage>
+								<UserInfo>
+									<Username>{user.full_name}</Username>
+									<Email>{user.email}</Email>
+								</UserInfo>
+							</UserContent>
+						</UserContainer>
+					))}
+			</UserOptions>
+		</UserPickerModalContainer>
+	);
+};
+
+export default UserListModal;
